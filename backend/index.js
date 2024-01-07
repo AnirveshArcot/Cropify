fa47ce323df6f9e425d3db10c0bf751a483afebc
Cyanide <77383465+AnirveshArcot@users.noreply.github.com>
2024-01-07 10:36:46 +0530

    Change CORS

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app=express();
 app.use(express.json({ limit: '10mb' }));
 app.use(
   cors({
-    origin: 'https://cropifyapp.netlify.app/',
+    origin: 'https://cropifyapp.netlify.app',
     credentials: true,
   })
 );
